cb4e969593e31a5c727fddfface290dd96b01683
mahnoorfaisalkhan241-collab <mahnoorfaisalkhan241@gmail.com>
2026-02-28 00:54:19 -0800

    2 chapters done

---
+++ chapter1/app.js
@@ -0,0 +1,17 @@
+// chap 1
+// Ans 1
+alert("hello");
+
+// Ans 2
+alert("Error!Please enter a valid message")
+
+// Ans 3
+ alert ("Wellcome to Js land \n  happy coding ")
+ 
+//  Ans 4
+alert ("Welcome to js land")
+alert ("Happy coding! \n  Prevent this page from creating additional dialogues ")
+
+// Ans 5
+console.log("Hello ... I can run JS through my web browser console ")
+  
---
+++ chapter2/app.js
@@ -0,0 +1,32 @@
+// chapter2 question
+// Answerno1
+let username =""
+// Answerno2
+let myName ="Mahnoorfaisalkhan"
+// Answerno3
+let message = "Hello World"
+alert (message);
+// Answerno4
+let studentname ="John Doe"
+let Age ="15 years old"
+let Course ="Certifited Mobile Application Development"
+alert (studentname);
+alert (Age);
+alert (Course);
+// Answerno5
+let food = "PIZZA\n PIZZ\n PIZ\n PI\n P"
+alert(food)
+// Answerno6
+let email ="mahnoorfaisal@gmail.com"
+alert ("My email address is "+" "+email)
+// Answerno7
+let book ="A smarter way to learn JavaScript"
+alert ("I am trying to learn from the book"+" "+book)
+// Answerno8
+document.write("Yah!I can write HTML content through JavaScript")
+let store ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” "
+alert(store);
+
+
+
+
