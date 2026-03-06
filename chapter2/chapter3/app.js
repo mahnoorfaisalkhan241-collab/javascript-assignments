2c3ede4218962b16e2753c5b843c4d8c0637000a
mahnoorfaisalkhan241-collab <mahnoorfaisalkhan241@gmail.com>
2026-03-06 14:38:05 -0800

    chapter4 done

---
+++ chapter2/chapter3/app.js
@@ -0,0 +1,10 @@
+// Chapter3 answer
+// Answer1
+var Age= 15
+alert ("I  years old");
+
+// Answer2
+
+// Answer3
+var birthYear = 2004
+document.writ
---
+++ chapter2/chapter3/chapter4/app.js
@@ -0,0 +1,21 @@
+// chapter4VariablesName
+
+// Answer1
+let userName ,userMail, userAge
+ 
+// Answer2
+let legal , $legal ,_legal ,legal_name ,legal1
+// let 1illegal , @illegal , illegal name , *illegal ,illegal- student
+
+// Answer3
+// a
+document.write("Rules for naming Js variables")
+// b
+let $my_1stVariable
+// c
+let $maha ,_maha ,maha
+// d
+sensitive
+// e
+keywords
+
